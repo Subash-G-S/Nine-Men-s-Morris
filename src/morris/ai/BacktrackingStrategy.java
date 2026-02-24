c5bb63b41adcaf774be87fc69250a2eb6e98803e
G S Subash Chandra Bose - [CB.SC.U4CSE24117] <cb.sc.u4cse24117@AMRITANET.EDU>
2026-02-24 15:42:08 +0530

    Backtracking logic for our game

--- src/morris/ai/BacktrackingStrategy.java
+++ src/morris/ai/BacktrackingStrategy.java
@@ -1,1 +1,11 @@
 //-------------------------BacktrackingImplementation-------------------------------------
+
+
+// LOGIC : 
+/*
+--------1 ) Choose the piece : picka legal move (placing a piece or moving a piece or flying a piece)
+--------2 ) Execute : Update the board state and check whether a mill is formed or not.
+--------3 ) Recursion : Call the function again and again to simulate the oponents best move .
+--------4 ) Backtrack : Revert the board to its previous state to check the alternative path . 
+ * 
+ */
